6fa8fb46eeb92d0866b54a3b672ceb12ce06b83a
thanniruKarthi <111129359+thanniruKarthi@users.noreply.github.com>
2022-08-18 13:13:25 +0530

    Create word-occurence.js

---
+++ word-occurence.js
@@ -0,0 +1,3 @@
+let x=`Paragraphs are medium-sized units of writing, longer than sentences, but shorter than sections, chapters, or entire works. Because they connect the "small" ideas of individual sentences to a "bigger" idea, paragraph structure is essential to any writing for organization, flow, and comprehension. Students have a lot of questions when it comes to writing a paragraph: How many sentences should you use? How do you transition within a paragraph? When do you end a paragraph? Etc. Below we explain everything you need to know about paragraph structure to write like an expert, including several paragraph examples.`
+let str=x.match(/paragraph/gi);
+console.log(str.length);
