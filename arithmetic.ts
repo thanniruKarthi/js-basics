cce4be6cba8235f324837f4664b6ec93f5cef613
thanniruKarthi <111129359+thanniruKarthi@users.noreply.github.com>
2022-08-25 11:15:03 +0530

    Create arithmetic.ts

---
+++ arithmetic.ts
@@ -0,0 +1,26 @@
+function arithmetic()
+{
+    function addition(a: number, b: number) 
+    {
+    return a + b;
+}
+function subtraction(a: number, b: number){
+    return a - b;
+  }
+  function division(a: number,b: number){
+
+return a / b;
+
+}
+function multiply(a: number, b: number) {
+    return a * b;
+  }
+  console.log(addition(2,5));
+
+  console.log(subtraction(2,5));
+
+  console.log(division(2,5));
+
+  console.log(multiply(2,5));
+}
+arithmetic();
